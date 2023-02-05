7f64f192d2f81126bd3cf92d31135bb351ac0d25
Dmitiy Gulevskiy <gulevskydev@yandex-team.ru>
2023-02-05 12:43:29 +0400

    front

---
+++ bfe/40. implement Bubble Sort.js
@@ -0,0 +1,14 @@
+
+/**
+ * @param {number[]} arr
+ */
+function bubbleSort(arr) {
+  for (let i=0; i<arr.length; i++) {
+    for (let j=i+1; j<arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        [arr[i], arr[j]] = [arr[j], arr[i]]
+      }
+    }
+  }
+  return arr
+}
